8daebb226825b2a8e2d03734df345bbfbb836456
YuanJ-i <jiyuan2048@gmail.com>
2017-03-17 15:15:12 -0400

    Add files via upload
    change some places

--- code/GP1_3.py
+++ code/GP1_3.py
@@ -9,6 +9,7 @@ import random
 import matplotlib.pyplot as plt
 from copy import copy
 
+
 #print(grid)
 #plt.imshow(grid)
 def successors(point,grid):
@@ -39,12 +40,10 @@ def diffuse(interval,sample=24):
     for i in range(80,120):
         for j in range(80):
             grid[i][j]=2
-                
-    
     time=interval*sample #whole time range
     grids=[]
-    temp=copy(grid)
-    grids.append(temp)
+    #temp=copy(grid)
+    grids.append(copy(grid))
     
     for k in range(time):
         if (k+1)%interval==0:
@@ -67,13 +66,13 @@ def diffuse(interval,sample=24):
     return grids
 #part a and part b ##########################################################################
 start=time.clock()
-grids=diffuse(15000000)
+grids=diffuse(1500000)
 end=time.clock()
 print str(end-start), "seconds"
 
 if not os.path.exists("p3ab/density_plot/densityalonglongside"):
         os.makedirs("p3ab/density_plot/densityalonglongside")
-if not os.path.exists("p3ab/density_plot/densityalongshortside"):
+if not os.path.exists("p3ab/density-_plot/densityalongshortside"):
         os.makedirs("p3ab/density_plot/densityalongshortside")
 if not os.path.exists("p3ab/grid_snapshots"):
         os.makedirs("p3ab/grid_snapshots")
@@ -86,7 +85,7 @@ for i,grid in enumerate(grids):
     plt.xlabel("x")
     plt.ylabel("y")
     plt.title("Gas snapshot"+str(i))
-    plt.savefig("p3ab/grid_snapshots/snapshot"+str(i)+".png")
+    plt.savefig("p3ab/grid_snapshots/snapshot"+str(i)+".pdf")
     
     plt.close(f1)
     
@@ -143,13 +142,10 @@ collection=[]
 
 start=time.clock()
 for r in range(repeats):
-    collection.append(diffuse(15000000))
-    #collection.append(diffuse(10000))
-end=time.clock()
-print str(end-start), "seconds"
+    collection.append(diffuse(1500000))
+    #collection.append(diffuse(100000))
 
 
-grids_aver=np.mean(collection,axis=0)
 
 if not os.path.exists("p3c/density_plot/densityalonglongside"):
         os.makedirs("p3c/density_plot/densityalonglongside")
@@ -157,63 +153,87 @@ if not os.path.exists("p3c/density_plot/densityalongshortside"):
         os.makedirs("p3c/density_plot/densityalongshortside")
 if not os.path.exists("p3c/grid_snapshots"):
         os.makedirs("p3c/grid_snapshots")
-for i,grid in enumerate(grids_aver):
 
-    #plot grid snapshot
-    f1=plt.figure()
-    plt.imshow(grid)
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.title("Gas snapshot"+str(i))
-    plt.savefig("p3c/grid_snapshots/snapshot"+str(i)+".png")
-    
-    plt.close(f1)
-    
-    
-    #calculate linear density for A and B along x,y directions
-    row_A=[]
-    row_B=[]
-    for j in range(120):
-        row=[0,0] # of A and B
-        for k in range(80):
-            if grid[j][k]==1:
-                row[0]+=1
-            elif grid[j][k]==2:
-                row[1]+=1
-        row_A.append(row[0])
-        row_B.append(row[1])
+row_A_aver2=[]
+row_B_aver2=[]
+col_A_aver2=[]
+col_B_aver2=[]
+for n,c in enumerate(collection):
+    row_A_aver=[]
+    row_B_aver=[]
+    col_A_aver=[]
+    col_B_aver=[]
+    for i,grid in enumerate(c):# snapshots--time
+        #calculate linear density for A and B along x,y directions
+        row_A=[]
+        row_B=[]
+        for j in range(120):
+            row=[0,0] # of A and B
+            for k in range(80):
+                if grid[j][k]==1:
+                    row[0]+=1
+                elif grid[j][k]==2:
+                    row[1]+=1
+            row_A.append(row[0])
+            row_B.append(row[1])
+        row_A_aver.append(row_A)
+        row_B_aver.append(row_B)
         
+        
+        col_A=[]
+        col_B=[]
+        for j in range(80):
+            #print j
+            col=[0,0] # of A and B
+            for k in range(120):
+                if grid[k][j]==1:
+                    col[0]+=1
+                elif grid[k][j]==2:
+                    col[1]+=1
+            col_A.append(col[0])
+            col_B.append(col[1])
+        col_A_aver.append(col_A)
+        col_B_aver.append(col_B)
+    row_A_aver2.append(row_A_aver)
+    row_B_aver2.append(row_B_aver)
+    col_A_aver2.append(col_A_aver)
+    col_B_aver2.append(col_B_aver)
+row_A_average=np.mean(row_A_aver2,axis=0)
+row_B_average=np.mean(row_B_aver2,axis=0)
+col_A_average=np.mean(col_A_aver2,axis=0)
+col_B_average=np.mean(col_B_aver2,axis=0)
+
+end=time.clock()
+print str(end-start), "seconds"
+'''
+plot and check result
+'''
+for i,(a,b) in enumerate(zip(row_A_average, row_B_average)):
     f2=plt.figure()
-    plt.plot(row_A,'.')
-    plt.plot(row_B,'.')
+    if int(np.sum(a))!=3200 or int(np.sum(b))!=3200:
+        print "wrong",np.sum(a),np.sum(b)
+    #print "sanpshot",i,"A # sum",np.sum(a),"should be 3200"  #40*80
+    #print "sanpshot",i,"B # sum",np.sum(b),"should be 3200"
+    plt.plot(a,'.')
+    plt.plot(b,'.')
     plt.legend(["A","B"],loc='upper right')
     plt.xlabel("y")
     plt.ylabel("density")
     plt.title("Density along y axis")
-    plt.savefig("p3c/density_plot/densityalonglongside/ydensity"+str(i)+".png")
+    plt.savefig("p3c/density_plot/densityalonglongside/ydensity"+str(i)+".pdf")
     plt.close(f2)
     
-    col_A=[]
-    col_B=[]
-    for j in range(80):
-        #print j
-        col=[0,0] # of A and B
-        for k in range(120):
-            if grid[k][j]==1:
-                col[0]+=1
-            elif grid[k][j]==2:
-                col[1]+=1
-        col_A.append(col[0])
-        col_B.append(col[1])
-
+for i,(a,b) in enumerate(zip(col_A_average, col_B_average)):
     f3=plt.figure()
-    plt.plot(col_A,'.')
-    plt.plot(col_B,'.')
+    if int(np.sum(a))!=3200 or int(np.sum(b))!=3200:
+        print "wrong",np.sum(a),np.sum(b)
+    #print "sanpshot",i,"A # sum",np.sum(a),"should be 3200"  #40*80
+    #print "sanpshot",i,"B # sum",np.sum(b),"should be 3200"
+    plt.plot(a,'.')
+    plt.plot(b,'.')
     plt.legend(["A","B"],loc='upper right')
     plt.xlabel("x")
     plt.ylabel("density")
     plt.title("Density along x axis")
-    plt.savefig("p3c/density_plot/densityalongshortside/xdensity"+str(i)+".png")
+    plt.savefig("p3c/density_plot/densityalongshortside/xdensity"+str(i)+".pdf")
     plt.close(f3)
-
-
